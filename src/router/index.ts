390e38344dc819c6213f1ebbca12aec61f536e47
Avinoam Sebbah <avinoam.sebbah@gmail.com>
2024-09-26 11:15:49 +0300

    qrcode integration

--- src/router/index.ts
+++ src/router/index.ts
@@ -14,6 +14,11 @@ const router = createRouter({
                     component: () => import('@/views/Dashboard.vue')
                 },
                 {
+                    path: '/qrcode',
+                    name: 'qrcode',
+                    component: () => import('@/views/pages/Qrcode.vue')
+                },
+                {
                     path: '/uikit/formlayout',
                     name: 'formlayout',
                     component: () => import('@/views/uikit/FormLayout.vue')
